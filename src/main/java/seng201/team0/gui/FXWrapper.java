2413d4f386ac74553df899c229a7397d31aede19
Lachie <lan89@uclive.ac.nz>
2024-05-15 20:52:43 +1200

    Added FXWrapper, GameEnvironment, SetupScreen.

---
+++ src/main/java/seng201/team0/gui/FXWrapper.java
@@ -0,0 +1,11 @@
+package seng201.team0.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class FXWrapper {
+    @FXML
+    private Pane pane;
+    private Stage stage;
+}
