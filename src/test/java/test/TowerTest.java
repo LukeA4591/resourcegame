9dd2972bcc3619e6970e0fa6a633177c830727dd
Lachie <lan89@uclive.ac.nz>
2024-05-23 13:49:30 +1200

    added test module

---
+++ src/test/java/test/TowerTest.java
@@ -0,0 +1,35 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import seng201.team0.models.towers.*;
+
+public class TowerTest {
+
+    private Tower armoury;
+    private Tower arsenal;
+    private Tower barracks;
+    private Tower garrison;
+    private Tower medicalTent;
+    private Tower medbay;
+    private Tower eaglesNest;
+    private Tower specialForcesCamp;
+    private Tower fieldHospital;
+
+
+    @BeforeEach
+    public void init() {
+        armoury = new Armoury();
+        arsenal = new Arsenal();
+        barracks = new Barracks();
+        garrison = new Garrison();
+        medicalTent = new MedicalTent();
+        medbay = new Medbay();
+        eaglesNest = new EaglesNest();
+        specialForcesCamp = new SpecialForcesCamp();
+        fieldHospital = new FieldHospital();
+    }
+
+}
