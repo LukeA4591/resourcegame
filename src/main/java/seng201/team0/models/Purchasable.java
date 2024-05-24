1a2d46b10ec68378f91f23a4d42772cf91d3102c
Lachie <lan89@uclive.ac.nz>
2024-05-25 05:43:12 +1200

    Javadoc

--- src/main/java/seng201/team0/models/Purchasable.java
+++ src/main/java/seng201/team0/models/Purchasable.java
@@ -1,8 +1,34 @@
 package seng201.team0.models;
 
+/**
+ * Represents a tower or item that can be bought in the game.
+ */
 public interface Purchasable {
+    /**
+     * Gets the cost of the item or tower.
+     *
+     * @return an int, the cost.
+     */
     int getCost();
+
+    /**
+     * Gets the sell price of the item or tower.
+     *
+     * @return an int, the sell price.
+     */
     int getSellPrice();
+
+    /**
+     * Gets the description of the item or tower.
+     *
+     * @return a String, the description.
+     */
     String getDescription();
+
+    /**
+     * Gets the name of the item or tower.
+     *
+     * @return a String, the name.
+     */
     String getName();
 }
--- src/main/java/seng201/team0/models/RandomEvent.java
+++ src/main/java/seng201/team0/models/RandomEvent.java
@@ -1,9 +1,24 @@
 package seng201.team0.models;
 
+/**
+ * Represents the types of random events that can occur in a game.
+ */
 public enum RandomEvent {
+    /**
+     * Breaks a player's tower.
+     * Higher chance of breaking towers using in last round.
+     */
     ENEMY_AIR_STRIKE,
+    /**
+     * Enemy ambush reduces the reload speed of an ammunition tower.
+     */
     ENEMY_AMBUSH,
+    /**
+     * Communications breakdown reduces the reload speed of a troop tower.
+     */
     COMMUNICATIONS_BREAKDOWN,
+    /**
+     * medical supply line sabotage reduces the reload speed of a medkit tower.
+     */
     MEDICAL_SUPPLY_LINE_SABOTAGE
-
 }
