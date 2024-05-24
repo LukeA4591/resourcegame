89c285e912f8a814609042ecabafb85b599a8f75
lukearmstrong <lar93@uclive.ac.nz>
2024-05-25 07:23:15 +1200

    javadoccing, commenting and small code improvements

--- src/main/java/seng201/team0/models/carts/AmmunitionCart.java
+++ src/main/java/seng201/team0/models/carts/AmmunitionCart.java
@@ -1,12 +1,25 @@
 package seng201.team0.models.carts;
 
+/**
+ * A cart that holds resource type ammunition
+ */
 public class AmmunitionCart extends Cart {
     private final String resourceType;
 
+    /**
+     * construct an instance of ammunition cart.
+     * @param size capacity of the cart
+     * @param speed speed of the cart.
+     */
     public AmmunitionCart(final int size, final int speed) {
         super(size, speed);
         this.resourceType = "Ammunition";
     }
+
+    /**
+     *
+     * @return get cart resource type
+     */
     public String getResourceType() {
         return resourceType;
     }
--- src/main/java/seng201/team0/models/carts/TroopCart.java
+++ src/main/java/seng201/team0/models/carts/TroopCart.java
@@ -1,12 +1,23 @@
 package seng201.team0.models.carts;
 
+/**
+ * A cart that holds resource type troops.
+ */
 public class TroopCart extends Cart {
     private final String resourceType;
-
+    /**
+     * construct an instance of troop cart.
+     * @param size capacity of the cart
+     * @param speed speed of the cart.
+     */
     public TroopCart(final int size, final int speed) {
         super(size, speed);
         this.resourceType = "Troops";
     }
+    /**
+     *
+     * @return get cart resource type
+     */
     public String getResourceType() {
         return resourceType;
     }
